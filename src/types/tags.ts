8e0758d9553602a4573d3e97feb42c5b0fb71dd8
marcelo.gomj <marelo.gomj@gmail.com>
2024-08-09 00:01:59 -0300

    add: main tags types

---
+++ src/types/tags.ts
@@ -0,0 +1,27 @@
+export type Tags  = {
+  album?: string,
+  composer?: string,
+  genre?: string,
+  copyright?: string,
+  date?: string,
+  title?: string,
+  mood?: string,
+  artist?: string,
+  partOfSet?: string,
+  publisher?: string,
+  trackNumber?: string,
+  year?: string,
+  comment?: {
+      language: string,
+      text: string,
+  },
+  image?: string | {
+      mime: string
+      type: {
+          id: number,
+          name?: string
+      },
+      description: string,
+      imageBuffer: Buffer,
+  }
+}
