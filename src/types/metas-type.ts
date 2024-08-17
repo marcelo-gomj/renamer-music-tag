fafeb0a739251c1c8236f2526e9e2c40d13e3d85
marcelo.gomj <marelo.gomj@gmail.com>
2024-08-17 01:32:52 -0300

    add: resumed tags

--- src/types/metas-type.ts
+++ src/types/metas-type.ts
@@ -1,14 +1,8 @@
-type MetasAsMethod = 
-  "trackNumber" | 
-  "title" | 
-  "year" | 
-  "feat" | 
-  "artist" | 
-  "partOfSet" | 
-  "album" ;
+import { Tags } from "./tags";
 
 
-type MetaKeys <T> =  { [key in MetasAsMethod] : T  }
+type MetasAsMethod = keyof Tags;
+type MetaKeys <T> =  { [key in MetasAsMethod] ?: T  }
 type MetaValue = { value: string, patternIndex ?: number} | null;
 type ObjectTransformer = MetaKeys<TransformMetaFunction>
 type MetaObjectResult = Partial<MetaKeys<MetaValue | null>> 
@@ -32,11 +26,15 @@ type ReturnMetas = Promise<{
   }
 }>
 
+type CurrentMetaSave = { [path: string] : {[ metakey in keyof Tags ] : MetaValue }} 
+
 export { 
   ObjectTransformer, 
   MetaObjectResult, 
+  CurrentMetaSave,
   MetasAsMethod, 
   MetaResult, 
   ReturnMetas ,
-  MetaKeys
+  MetaKeys, 
+  MetaValue
 }
