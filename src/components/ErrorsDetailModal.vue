6710ff975fce2194e614f957022ffb27e6f5dd53
marcelo.gomj <marcelomgj005@gmal.com>
2024-09-14 02:17:53 -0300

    update: new error list modal

--- src/components/ErrorsDetailModal.vue
+++ src/components/ErrorsDetailModal.vue
@@ -1,14 +1,13 @@
 <template>
-<div class="flex flex-col gap-3 relative text-x1.5 h-full overflow-y-hidden">
-  <div class="font-medium sticky top-0 px-2 bg-base-dark-250">Erro nos caminhos:</div>
-  
-  <div class="flex flex-col h-full w-full overflow-y-hidden">
-    <ul class="border-2 border-base-dark-400 space-y-1.5 p-2 overflow-y-scroll h-full ">
+<div class="relative text-x1.5 py-2 h-full overflow-y-hidden">  
+  <div class="flex flex-col px-2 py-4 h-full w-full rounded-xl overflow-y-hidden">
+    <ul class="space-y-3 px-2 pr-6 overflow-y-scroll h-full ">
       <li 
-        class
-        v-for="path of failedList"
+        class="group flex gap-4 items-center w-full font-medium text-x1 cursor-pointer"
+        v-for="path of paths"
       >
-        <div class="text-red-600 line-clamp-2">{{ path }}</div>
+          <div :class="[typeColor[type], ` line-clamp-2 group-hover:text-base-white-200`]">{{ path }}</div>
+          <ExternalLink class="size-4 invisible group-hover:visible" />
       </li>
     </ul>
   </div>
@@ -16,9 +15,15 @@
 </template>
 
 <script setup lang="ts">
-const { failedList, successfulList } = defineProps<{
-  successfulList : string[],
-  failedList : string[]
+import { ExternalLink } from "lucide-vue-next";
+const { paths, type } = defineProps<{
+  paths: string[],
+  type: 'error' | 'sucess'
 }>()
+
+const typeColor = {
+  'error' : 'text-red-500',
+  'sucess' : 'text-green-500'
+}
 
 </script>
