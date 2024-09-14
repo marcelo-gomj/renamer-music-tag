f527d453a4a89ed603e6d447c5e77543f024043c
marcelo.gomj <marcelomgj005@gmal.com>
2024-09-14 02:22:50 -0300

    fix: modal layout

--- src/components/ModalGlobal.vue
+++ src/components/ModalGlobal.vue
@@ -4,25 +4,29 @@
       class="flex z-[999] items-center overflow-hidden justify-center absolute w-full h-full bg-[rgba(10,10,10,0.4)] top-0 left-0"
       v-if="modalProps.content"
     >
-      <div class="border-2 border-base-dark-300 flex relative flex-col max-w-[800px] min-w-[600px] bg-base-dark-200 rounded-2xl">
-        <div class="flex justify-center py-3 items-center">
-          <div class="font-semibold line-clamp-1 py-2 tracking-wider text-x3">{{ modalProps.title }}</div>
+      <div class="border-base-dark-900 flex relative flex-col w-[700px] bg-base-dark-200 rounded-xl">
+        <div class="flex relative justify-center h-12 items-center">
+          <div 
+            class="font-semibold text-center w-full line-clamp-1 tracking-wider text-x3"
+            :title="modalProps.title"
+          >
+            {{ modalProps.title }}
+          </div>
     
-          
-          <div class="absolute right-2 p-2 opacity-70 rounded-full hover:scale-110 cursor-pointer"
+          <div class="flex absolute items-center h-full right-0 px-4  opacity-70 hover:scale-110 cursor-pointer"
             @click="_ => close()"
           >
-            <X class="size-5" />
+            <X class="size-6 stroke-[2.5]" />
           </div>
         </div>
     
-        <div class="flex flex-col gap-4 relative w-full py-4 px-8 overflow-y-hidden">
-          <div class="min-h-52 h-[50vh] overflow-y-hidden">
+        <div class="flex flex-col gap-4 relative w-full py-4 px-6 overflow-y-hidden">
+          <div class="min-h-52 h-[50vh] overflow-hidden px-2">
             <component :is="modalProps.content"  />
           </div>
     
           <div class="flex shrink-0 gap-6 overflow-hidden justify-center w-full text-x2 font-medium ">
-            <div class="py-2 w-40 text-center border-[1.5px] border-base-dark-700 hover:border-white bg-base-dark-300 rounded-full cursor-pointer transition-[background-color_150ms_ease]"
+            <div class="py-2 w-44 text-center border-[1.5px] border-base-dark-700 hover:border-white bg-base-dark-300 rounded-full cursor-pointer transition-[background-color_150ms_ease]"
               v-if="modalProps.firstButton"
               @click="modalProps.firstButton.fnAction"
             >
