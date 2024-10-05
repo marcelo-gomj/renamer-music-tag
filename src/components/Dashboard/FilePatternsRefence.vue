9fe9ebd697ddf9e2ae9fde86f7249b0465d80fad
marcelo.gomj <marcelo.gomj@gmail.com>
2024-10-04 23:43:35 -0300

    fix: get all file references

--- src/components/Dashboard/FilePatternsRefence.vue
+++ src/components/Dashboard/FilePatternsRefence.vue
@@ -4,16 +4,18 @@
       <div class="font-medium text-x1 pb-4">Referência dos arquivos</div>
 
       <div class="flex flex-wrap font-medium gap-x-2 gap-y-4 pr-4 w-full my-1 min-h-[2.2rem]">
-        <div v-if="metadatasGenereted" 
-          v-for="pattern in patternsTagged()" 
-          :key="metadatasGenereted[0].path" 
-          :class="`flex tracking-wide items-center select-none group gap-4 ${type(pattern) === 'Array'  ? 'pl-5 pr-6 rounded-full cursor-pointer text-x1 bg-base-dark-400 hover:bg-base-dark-600 shadow-[2px_2px_20px] shadow-base-dark-200' : 'px-1.5 text-x2 font-bold text-base-white-800'}`">
+        <div 
+          v-if="metadatasGenereted" 
+          v-for="(pattern, index) in currentPattern" 
+          :key="(R.is(String, pattern) ? pattern : pattern.tagName) + index "
+          :class="`flex tracking-wide items-center select-none group gap-4 ${'pl-5 pr-6 rounded-full cursor-pointer text-x1 bg-base-dark-400 hover:bg-base-dark-600 shadow-[2px_2px_20px] shadow-base-dark-200'}`"
+        >
 
-          <component :is="type(pattern) === 'Array'  ? METADATAS[pattern[0]].icon : null" class="w-[1rem] h-[1rem]" />
+          <component :is="R.is(String, pattern) ? '' : pattern.icon" class="w-[1rem] h-[1rem]" />
 
-          <div class="leading-[1]">{{ is(Array, pattern) ? (METADATAS[pattern[0]].label || pattern[0]): pattern }}</div>
+          <div class="leading-[1]">{{ R.is(String, pattern) ? pattern : (pattern.label || pattern.tagName)}}
+          </div>
         </div>
-
         <div v-else class="flex justify-between rounded-lg h-full flex-col w-full ">
           <div class="opacity-30 py-1 px-4"></div>
         </div>
@@ -23,34 +25,106 @@
 </template>
 
 <script setup lang="ts">
-import { Music2, Captions, Disc, CalendarIcon, Tag, LucideUsers } from 'lucide-vue-next';
-import { toPairs, update, type, is } from "ramda";
+import { Music2, Captions, Disc, CalendarIcon, Tag, LucideUsers, Users } from 'lucide-vue-next';
+import * as R from "ramda";
 import { useMedatas } from '@/stores/metadatas';
 import { storeToRefs } from 'pinia';
+import { PatternList, ReducePatternsObject, ReferencePathsPattern, ReferencePatterns } from '@/types/vue-types'
+import { ref, watch } from 'vue';
+import { GenMetadatasResult, GenTagKey } from '@/types/metas-type';
+import { usePathSelection } from '@/stores/path-selections';
 
-const { metadatasGenereted } =  storeToRefs(useMedatas());
+type ReducedPairsPattern = [string, number];
 
-function patternsTagged() : (string | [string])[] {
-  if(!metadatasGenereted.value.length) return [];
+const { metadatasGenereted, currentMetadatas } = storeToRefs(useMedatas());
+const { pathSelections } = storeToRefs(usePathSelection());
 
-  if(metadatasGenereted.value.length){
-    return toPairs(metadatasGenereted.value[0].metadatas)
-    .reduce((patterns, [tag, { patternIndex }] ) => {
-      return update<string | [string]>(patternIndex, [tag], patterns);
-    }, metadatasGenereted.value[0].patterns )
+const pathReferences = ref<ReferencePathsPattern>({})
+const patternReferences = ref<ReferencePatterns>({})
+const currentPattern = ref<PatternList>([]);
+
+const getPatternWithMostFrequence = (pathsObj: ReferencePathsPattern) => R.pipe(
+  R.countBy(R.identity<string>),
+  R.toPairs,
+  R.reduce<ReducedPairsPattern, ReducedPairsPattern>((a, b) => 
+    (a[1] > b[1] ? a : b),
+    ['', 0]
+  ),
+  R.head
+)(R.values(pathsObj)) as string;
+
+function watchReferenceMetadatas(metadatasGenereted: GenMetadatasResult[]) {
+  const { patterns, pathIndexedByPatterns} =   metadatasGenereted.reduce(
+    ({ patterns = {} , pathIndexedByPatterns = {} }, { metadatas, path }, index) => {
+      const tagList = R.keys(metadatas);
+      const patternKey = 'p' + index;
+      
+      R.forEachObjIndexed((pattern, patternId) => {
+        if(R.equals(tagList, pattern)){
+          pathIndexedByPatterns[path] = String(patternId);
+        }
+      }, patterns)
+
+      if(!R.hasIn(path, pathIndexedByPatterns)){
+        patterns[patternKey] = tagList;
+        pathIndexedByPatterns[path] = patternKey;
+      }
+      
+      return { pathIndexedByPatterns, patterns }
+    }, 
+    {} as ReducePatternsObject
+  )
+
+  pathReferences.value = pathIndexedByPatterns;
+  patternReferences.value = patterns;
+  // currentPattern.value = getPatternWithMostFrequence(pathIndexedByPatterns);
+  const popularPattern =  getPatternWithMostFrequence(pathIndexedByPatterns) as GenTagKey;
+  currentPattern.value = patterns[popularPattern];
+}
+
+
+
+
+const METADATAS: { [key in GenTagKey] ?: { label: string, icon: typeof Music2 } } = {
+  "trackNumber": { label: "Faixa", icon: Music2 },
+  "title": { label: "Título", icon: Captions },
+  "album": { label: "Álbum", icon: Disc },
+  "artist": { label: "Artista", icon: LucideUsers },
+  "partOfSet": { label: "Disco de ", icon: Disc },
+  "year": { label: 'Ano', icon: CalendarIcon },
+  "genre": { label: 'Genêro', icon: Tag },
+  "performerInfo": { label: 'Artistas', icon: Users }
+}
+
+function watchPathsReferece(paths: Set<string>){
+  const pathsArray = [...paths.keys()];
+  
+  if(paths.size === 1){
+    const meta = R.find( meta => meta.path === pathsArray[0] , metadatasGenereted.value);
+    console.log(meta)
+    
+    currentPattern.value = R.reduce((patterns, [tagName, { patternIndex }]) => (
+      R.update(patternIndex, { tagName, ...METADATAS[tagName] }, patterns)
+    ),
+      meta.patterns as PatternList,
+      R.toPairs(meta.metadatas)
+    )
+      
+    return;
   }
+  
+  const refs = R.pick(
+    pathsArray.length === 0 ? R.keys(pathReferences.value) : pathsArray, 
+    pathReferences.value
+  );
 
-  return [];
+  const patternKey = getPatternWithMostFrequence(refs) as GenTagKey;
+  currentPattern.value =  R.map( tagName => ({ 
+    tagName,
+     ...METADATAS[tagName]}
+    ), patternReferences.value[patternKey] || [])
 }
 
-const METADATAS : {[ key : string] : { label: string, icon: typeof Music2 }} = {
-  "trackNumber" : { label: "Faixa", icon: Music2 } ,
-  "title" : { label: "Título", icon: Captions } ,
-  "album" : { label: "Álbum", icon: Disc } ,
-  "artist" : { label: "Artista", icon: LucideUsers },
-  "partOfSet" : { label: "Disco de ", icon: Disc },
-  "year":  { label: 'Ano', icon: CalendarIcon },
-  "genre" : { label: 'Genêro', icon: Tag},
-  "default" : { label: 'Genêro', icon: Tag},
-}
+watch(metadatasGenereted, watchReferenceMetadatas);
+watch(pathSelections, watchPathsReferece, { deep : true});
 </script>
--- src/types/vue-types.ts
+++ src/types/vue-types.ts
@@ -1,33 +1,32 @@
 import { FunctionalComponent, h } from "vue"
-import { GenMetadatasResult } from "./metas-type"
+import { GenMetadatasResult, GenTagKey } from "./metas-type"
 import { Tags } from "./tags"
 
 export type SourceSelectProps = {
   currentDir: string,
-  addSourceDir : (sourcePath: string[]) => void,
-  updateMetaResults : (paths ?: string[]) => { 
-    error: { name: string, pathErrors: string[]}, 
+  addSourceDir: (sourcePath: string[]) => void,
+  updateMetaResults: (paths?: string[]) => {
+    error: { name: string, pathErrors: string[] },
     result: GenMetadatasResult[]
-  } 
+  }
 }
 
 export type RoutePath = (path: 'home' | 'dashboard' | 'config') => void
 
-export type ModalGlobalLayoutProps = { 
-  title ?: string, 
-  content : ReturnType<typeof h> | '',
-  firstButton ?: { label: string, fnAction: () => void },
-  secondButton ?: { label: string, fnAction: () => void },
-  closeModal ?: () => void
+export type ModalGlobalLayoutProps = {
+  title?: string,
+  content: ReturnType<typeof h> | '',
+  firstButton?: { label: string, fnAction: () => void },
+  secondButton?: { label: string, fnAction: () => void },
+  closeModal?: () => void
 }
 
-export type ProvideModalGlobal = (options : ModalGlobalLayoutProps) => void;
+export type ProvideModalGlobal = (options: ModalGlobalLayoutProps) => void;
+export type FieldTagStatus = "EDITED" | "GENERATED" | "DEFAULT";
 
-export type FieldTagStatus =  "EDITED" | "GENERATED" | "DEFAULT";
-
-export type FieldValue ={
-  tagValue: string, 
-  status : FieldTagStatus[]
+export type FieldValue = {
+  tagValue: string,
+  status: FieldTagStatus[]
 }
 
 export type FieldUniqueValue = {
@@ -37,13 +36,17 @@ export type FieldUniqueValue = {
 
 export type InputProps = Map<keyof Tags, FieldValue>;
 
-export type IndexPathTags <T> = { [path : string] : { [tag in keyof Tags]: T }};
+export type IndexPathTags<T> = { 
+  [path: string]: { 
+    [tag in keyof Tags]: T 
+  } 
+};
 
 export type InputDataProps = {
-  tag: keyof Tags, 
+  tag: keyof Tags,
   status: FieldTagStatus[],
-  value: string, 
-  icon : FunctionalComponent<any>
+  value: string,
+  icon: FunctionalComponent<any>
 }[]
 
 export type GlobalNotificationsProps = {
@@ -51,7 +54,25 @@ export type GlobalNotificationsProps = {
   id: number,
   type: 'SUCCESS' | 'ERROR' | 'ALERT',
   context?: string,
-  actionButton ?: (id: number) => void,
+  actionButton?: (id: number) => void,
 };
 
-export type SetNotificationFunction = (notificationsProps: GlobalNotificationsProps) => void;
+export type SetNotificationFunction = (
+  notificationsProps: GlobalNotificationsProps
+) => void;
+
+
+export type ReferencePathsPattern = {
+  [path: string] : string
+}
+
+export type ReferencePatterns = {
+  [pattern: string] : GenTagKey[]
+}
+
+export type ReducePatternsObject = {
+  pathIndexedByPatterns : ReferencePathsPattern,
+  patterns : ReferencePatterns
+}
+
+export type PatternList = (string | { tagName: GenTagKey, icon?: any,  label?: string,  })[]  
